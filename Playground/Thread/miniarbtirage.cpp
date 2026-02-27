86df332a5db073f7313298c487f2fcf101ecb727
Chisaiki <mishitarahman@gmail.com>
2026-02-26 20:38:05 -0500

    added design notes

--- Playground/Thread/miniarbtirage.cpp
+++ Playground/Thread/miniarbtirage.cpp
@@ -20,3 +20,8 @@ int main(){
 
 // Each thread should be generating a random number and
 // updating the price variables after the generate the number
+
+// a third thread will be reading the data as soon as it updates 
+// and calculates the difference to display
+
+// who should display it? should i focus on optimization?
